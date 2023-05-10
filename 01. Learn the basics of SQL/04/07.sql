6c461c5409d2733c844c609b5da4ce02acefa949
Grigory-Sh <grigory.sheverdin@gmail.com>
2023-05-10 21:29:32 +0300

    added 4th lesson

---
+++ 01. Learn the basics of SQL/04/07.sql
@@ -0,0 +1,17 @@
+/*
+Выберите из таблицы products id и наименования только тех товаров,
+названия которых начинаются на букву «с» и содержат только одно слово.
+Результат должен быть отсортирован по возрастанию id товара.
+Поля в результирующей таблице: product_id, name
+*/
+
+SELECT
+  product_id,
+  name
+FROM
+  products
+WHERE
+  name LIKE 'с%'
+  and name NOT LIKE '% %'
+ORDER BY
+  product_id ASC
